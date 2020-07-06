55104d00bee7ca2fb2809de6791fe06a4b85b505
Alfred Bullus <Alfred.Bullus@softwire.com>
2020-07-06 14:39:08 +0100

    Initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 4;
+            
+        }
+    }
+}
